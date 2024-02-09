51d400ebbb0e6867abe2ffa8a03bf4da44c7874d
SquallStrifeX <squallstrifex@gmail.com>
2024-02-09 17:16:13 +0100

    end bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
+<body><div class="container">
+    <div class="row">
+        <div class="col-12 mt-5">
+             <ul class="list-unstyled ">
+        <li class="list-item mt-3">
+            <a href="{{ route('tecniche') }}" class="btn btn-primary">Tecniche</a>
+        </li>
+        <li class="list-item mt-3">Qualità</li>
+        <li class="list-item mt-3">
+            <a href="{{ route('magia_bianca') }}" class="btn btn-primary">Magia Bianca</a>
+        </li>
+        <li class="list-item mt-3"> <a href="{{ route('magia_nera') }}" class="btn btn-primary">Magia Nera</a></li>
+        <li class="list-item mt-3">Invocazioni</li>
+    </ul>
    {{ $text }}
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,80 @@ Route::get('/', function () {
     $text = 'Hello World'; // Assegnare "Hello World" a una variabile
     return view('home', compact('text')); // Passare la variabile alla vista utilizzando compact()
 });
+
+Route::get('/tecniche', function () {
+    $tecniche = ["Ambliopya",
+    "Narcoipnosi",
+    "Silenziatore",
+    "Antivis",
+    "Antikelesis",
+    "Moviola",
+    "Ambliopya X",
+    "Aphathia",
+    "Narcoipnosi X",
+    "Scippo",
+    "Silenziatore X",
+    "Antimajix",
+    "Antiscutum",
+    "Moviola X",
+    "3 Penalità",
+    "Estorsione",
+    "Tokkata",
+    "Ganascia"];
+    return view('tecniche', compact('tecniche')); // Passare la variabile alla vista utilizzando compact()
+})->name('tecniche') ;
+
+Route::get('/magia_bianca', function () {
+    $magia_bianca = ['Energia',
+    'Energira',
+    'Energiga',
+    'Parafire',
+    'Parathund',
+    'Paraidro',
+    'Parabliz',
+    'Scan',
+    'Esna',
+    'Reiz',
+    'Areiz',
+    'Haste',
+    'Hastega',
+    'Slow',
+    'Slowga',
+    'Shell',
+    'Protect',
+    'Reflex',
+    'Dispel',
+    'Rigene',
+    'Sancta',
+    'Risveglio'];
+    return view('magia_bianca', compact('magia_bianca')); // Passare la variabile alla vista utilizzando compact()
+})->name('magia_bianca') ;
+
+Route::get('/magia_nera', function () {
+    $magia_nera = [
+        "Fire",
+        "Fira",
+        "Firaga",
+        "Thunder",
+        "Thundara",
+        "Thundaga",
+        "Idro",
+        "Idrora",
+        "Idroga",
+        "Blizzard",
+        "Blizzara",
+        "Blizzaga",
+        "Bio",
+        "Antima",
+        "Ade",
+        "Drain",
+        "Aspir",
+        "Flare",
+        "Ultima"
+    ]
+    ;
+    return view('magia_nera', compact('magia_nera')); // Passare la variabile alla vista utilizzando compact()
+})->name('magia_nera') ;
+
+
+
